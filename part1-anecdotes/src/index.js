748d649012eae3d16d53a239bdbc973857cdd0c9
Carlos Azaustre <cazaustre@gmail.com>
2019-06-14 22:58:31 +0200

    1.13 anecdotes step2

--- part1-anecdotes/src/index.js
+++ part1-anecdotes/src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom';
 
 const App = props => {
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(6).fill(0));
 
   const nextAnecdote = () => {
     const getRandomInt = Math.floor(Math.random() * Math.floor(6));
     setSelected(getRandomInt);
   }
-  
+
+  const voteAnecdote = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
+  }
+
   return (
     <div>
       <p>{props.anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <button onClick={voteAnecdote}>vote</button>
       <button onClick={nextAnecdote}>next anecdote</button>
     </div>
   )
