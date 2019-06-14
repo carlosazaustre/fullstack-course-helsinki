3dea952aa9f5214e415f785b35c042e81ede392d
Carlos Azaustre <cazaustre@gmail.com>
2019-06-14 21:55:37 +0200

    1.10: unicafe step5

--- part1-unicafe/src/index.js
+++ part1-unicafe/src/index.js
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+
+const Statistic = ({ text, value }) => {
+  return <p>{text} {value}</p>;
+}
+
 const Statistics = ({ good, neutral, bad }) => {
   let all = good + neutral + bad;
   let average = (good + bad * (-1)) / all;
   let positive = (good / all) * 100;
-
+  
   if ((good || neutral || bad) !== 0) {
     return (
       <>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={all} />
+        <Statistic text="average" value={average} />
+        <Statistic text="positive" value={positive} />
       </>
     );
   }
-
+  
   return <div>No feedback given</div>
 }
 
@@ -27,16 +32,27 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
+  
+  // const handleGood = () => setGood(good + 1);
+  const handleGood = () => console.log(good);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+  
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button text="good" onClickButton={handleGood} />
+      <Button text="neutral" onClickButton={handleNeutral} />
+      <Button text="bad" onClickButton={handleBad} />
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
+  )
+}
+const Button = ({ text, onClickButton }) => {
+  // return <Button onClick={handleClick}>{text}</Button>;
+  return (
+    <button onClick={onClickButton}>{text}</button>
   )
 }
 
