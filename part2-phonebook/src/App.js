f8afec3f5d147a479607062c1b671c26bf5dea15
Carlos Azaustre <cazaustre@gmail.com>
2019-06-16 15:19:27 +0200

    2.11: The Phonebook Step6

--- part2-phonebook/src/App.js
+++ part2-phonebook/src/App.js
@@ -1,21 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Carlos Azaustre', number: '12-34-56789' },
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchName, setSearchName] = useState('');
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      });
+  }
+  useEffect(hook, []);
 
   const contactsToShow = showAll
     ? persons
