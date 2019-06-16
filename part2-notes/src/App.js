045884a0e82458d7a5088f98fb5a18b211d13025
Carlos Azaustre <cazaustre@gmail.com>
2019-06-16 15:10:48 +0200

    part2.4

--- part2-notes/src/App.js
+++ part2-notes/src/App.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Note from './components/Note';
 
 const App = props => {
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log('effect');
+
+    const eventHandler = response => {
+      console.log('promise fullfilled');
+      setNotes(response.data);
+    }
+
+    const promise = axios.get('http://localhost:3001/notes');
+    promise.then(eventHandler);
+  };
+  useEffect(hook, []);
 
   const notesToShow = showAll
     ? notes
