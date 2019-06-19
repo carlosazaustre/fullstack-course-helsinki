771a3972ec67eeca87a750c8b376959e939a386a
Carlos Azaustre <cazaustre@gmail.com>
2019-06-19 23:08:01 +0200

    2.17: Phonebook step9 - 2

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
-const Persons = ({ persons, onDelete }) => (  
-  <>
-    {persons.map(pers => (
-      <p key={pers.id}>
-        {pers.name} {pers.number}
-        <button onClick={() => onDelete(pers.id)}>delete</button>
-      </p>
-    ))}
-  </>
-);
+const Persons = ({ persons, onDelete }) => {
+  function confirmDelete(id, name) {
+    if(window.confirm(`Delete ${name}?`)) {
+      onDelete(id);
+    }
+    return;
+  }
 
+  return(
+    <>
+      {persons.map(pers => (
+        <p key={pers.id}>
+          {pers.name} {pers.number}
+          <button onClick={() => confirmDelete(pers.id, pers.name)}>delete</button>
+        </p>
+      ))}
+    </>
+  );
+}
 export default Persons;
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -13,7 +13,7 @@ const create = newObject => {
 
 const remove = id => {
   const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then(response => response.data);
+  return request;
 }
 
 export default {
