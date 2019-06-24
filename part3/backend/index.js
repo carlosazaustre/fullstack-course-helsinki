a27642397f2858db85d3423ed1fd280354c87ca8
Carlos Azaustre <carlos.azaustre@ibm.com>
2019-06-25 00:29:17 +0200

    part3-1

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
 
 let notes = [
   {
@@ -22,6 +25,13 @@ let notes = [
   }
 ];
 
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1;
+}
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>');
 });
@@ -29,6 +39,43 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
   res.json(notes);
 });
+
+app.post('/notes', (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  };
+
+  notes = notes.concat(note);
+  res.json(note);
+})
+
+app.get('/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find(note => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete('/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter(note => note.id !== id);
+
+  res.status(204).end();
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {
