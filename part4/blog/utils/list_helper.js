0415bca7cfad0c0e0416a5df7d2184ed059e5b43
Carlos Azaustre <cazaustre@gmail.com>
2019-07-20 15:44:28 +0200

    4.5*: helper functions and unit tests, step3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -10,7 +10,26 @@ const totalLikes = (blogs) => {
       .reduce((sum, item) => sum + item);
 };
 
+const favoriteBlog = (blogs) => {
+  let likes = 0;
+  let mostLiked = {};
+
+  blogs.forEach((blog) => {
+    if (blog.likes > likes) {
+      // eslint-disable-next-line prefer-destructuring
+      likes = blog.likes;
+      mostLiked = blog;
+    }
+  });
+  return {
+    title: mostLiked.title,
+    author: mostLiked.author,
+    likes: mostLiked.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
