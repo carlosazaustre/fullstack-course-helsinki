f602a4c6d0e138c578d3773112e09b0e38d56298
Carlos Azaustre <cazaustre@gmail.com>
2019-07-27 11:31:40 +0200

    5.6* Blog list frontend, step6

--- part5/blog/frontend/src/components/Blog.js
+++ part5/blog/frontend/src/components/Blog.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? '': 'none' };
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setVisible(!visible)}>
+        {blog.title} - {blog.author}
+        <div style={showWhenVisible}>
+          <a href={blog.url}>{blog.url}</a><br />
+          {blog.likes} likes <button>like</button><br />
+          added by {blog.user.name}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
