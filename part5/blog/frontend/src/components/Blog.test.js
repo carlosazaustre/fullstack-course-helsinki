067c021e0cf61c95233db12287f7a16eb2596538
Carlos Azaustre <cazaustre@gmail.com>
2019-07-27 21:14:10 +0200

    4.14: Blog list tests, step2

---
+++ part5/blog/frontend/src/components/Blog.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import Blog from './Blog';
+
+afterEach(cleanup);
+
+describe('<Blog />', () => {
+  let component;
+  let mockHandleRemove;
+  let mockHandleUpdate;
+  const blog = {
+    title: 'Test title',
+    author: 'Test author',
+    likes: 1,
+    url: 'http://localhost.com',
+    id: '1',
+    user: {
+      name: 'John Doe',
+      username: 'johndoe',
+      id: '2'
+    }
+  };
+
+  beforeEach(() => {
+    mockHandleRemove = jest.fn();
+    mockHandleUpdate = jest.fn();
+    component = render(
+      <Blog
+        blog={blog}
+        handleRemove={mockHandleRemove}
+        handleUpdate={mockHandleUpdate}
+        showRemoveButton={true}
+      />
+    )
+  });
+
+  test('button of a component is pressed twice', () => {
+    const button = component.getByText('like');
+    fireEvent.click(button);
+    fireEvent.click(button);
+    expect(mockHandleUpdate.mock.calls.length).toBe(2);
+  });
+});
